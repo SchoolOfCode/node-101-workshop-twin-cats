879b8a9e8adf3e45d790d1f9ca60f59677159859
Katherine Tweeddale <Katweeddale@gmail.com>
2021-12-06 17:01:15 +0000

    node-101-bonus

--- index.js
+++ index.js
@@ -32,10 +32,10 @@ import myCollection from "./collection.js";
         // console.log(" I have " + myCollection[i].count + " item(s).");
         let itemCount = myCollection[i].count
         if (itemCount === 1) {
-          console.log("I have " + (chalk.yellow(myCollection[i].count)) + " " + (chalk.cyan(myCollection[i].name)) + ". Here's what I like about it: " + (chalk.green(myCollection[i].whatILike)));
+          console.log("I have " + (chalk.bgMagenta.yellow(myCollection[i].count)) + " " + (chalk.cyan.bold(myCollection[i].name)) + ". Here's what I like about it: " + (chalk.inverse.green.bgWhite(myCollection[i].whatILike)));
         }
         else if (itemCount > 1) {
-          console.log("I have " + (chalk.yellow(myCollection[i].count)) + " " + (chalk.cyan(myCollection[i].name + "s")) + ". Here's what I like about them: " + (chalk.green(myCollection[i].whatILike)));
+          console.log("I have " + (chalk.bgMagenta.yellow(myCollection[i].count)) + " " + (chalk.bold.cyan(myCollection[i].name + "s")) + ". Here's what I like about them: " + (chalk.bgWhite.inverse.green(myCollection[i].whatILike)));
         }
       }
   }
@@ -55,3 +55,9 @@ function describeCollection(array) {
 describeCollection(myCollection)
 
 //4 - NPM and node packages
+// chalk 
+
+// bonus tasks
+
+console.log(chalk.bgRgb(15, 100, 204).inverse('Hello!'));
+console.log(chalk.bgRgb(150, 10, 204)('Hello!'));
