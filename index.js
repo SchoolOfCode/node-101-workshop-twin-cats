47d06921faa695d8d472326077d5bdf2343a4f46
Katherine Tweeddale <Katweeddale@gmail.com>
2021-12-06 16:40:27 +0000

    node-101-task4

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 //3 
-// import myCollection from "./collection.js";
+import chalk from "chalk";
+import myCollection from "./collection.js";
 
 // 2b
 
-let myCollection = [
-  {
-    name: "School of Code mug",
-    count: 1,
-    whatILike: "It has my cute pixel character on it!"
-  },
-  {
-    name: "School of Code hat",
-    count: 2,
-    whatILike: "An often overlooked fashion accessory"
-  },
-  {
-    name: "School of Code pillow",
-    count: 1,
-    whatILike: "Eat. Sleep. Code. Repeat :)"
-  }
-];
+// let myCollection = [
+//   {
+//     name: "School of Code mug",
+//     count: 1,
+//     whatILike: "It has my cute pixel character on it!"
+//   },
+//   {
+//     name: "School of Code hat",
+//     count: 2,
+//     whatILike: "An often overlooked fashion accessory"
+//   },
+//   {
+//     name: "School of Code pillow",
+//     count: 1,
+//     whatILike: "Eat. Sleep. Code. Repeat :)"
+//   }
+// ];
 
 
   //console.log(myCollection);
@@ -31,16 +32,17 @@ let myCollection = [
         // console.log(" I have " + myCollection[i].count + " item(s).");
         let itemCount = myCollection[i].count
         if (itemCount === 1) {
-          console.log("I have a " + myCollection[i].name + ". Here's what I like about it: " + myCollection[i].whatILike);
+          console.log("I have " + (chalk.yellow(myCollection[i].count)) + " " + (chalk.cyan(myCollection[i].name)) + ". Here's what I like about it: " + (chalk.green(myCollection[i].whatILike)));
         }
         else if (itemCount > 1) {
-          console.log("I have " + myCollection[i].name + "s. Here's what I like about them: " + myCollection[i].whatILike);
+          console.log("I have " + (chalk.yellow(myCollection[i].count)) + " " + (chalk.cyan(myCollection[i].name + "s")) + ". Here's what I like about them: " + (chalk.green(myCollection[i].whatILike)));
         }
       }
   }
 }
 
-describeItem("School of Code mug")
+
+// describeItem("School of Code mug")
 
 
 
@@ -50,6 +52,6 @@ function describeCollection(array) {
   }
 }
 
-// describeCollection(myCollection)
+describeCollection(myCollection)
 
 //4 - NPM and node packages
